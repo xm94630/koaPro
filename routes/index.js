75e097150e00d3bf846a27f9f146ca24f0159ef8
xm94630 <xm94630@126.com>
2019-02-27 23:22:59 +0800

    初始化koa项目

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/test', async (ctx, next) => {
+
+  const a = await new Promise((resolve)=>{
+    setTimeout(function(){
+      resolve(123)
+    },3000)
+  })
+
+
+  ctx.body = {
+    a
+  }
+})
+
+module.exports = router
